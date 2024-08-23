dbcf3ac0b49177b025f4e7b99dd8724314f7d14f
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-22 21:23:02 -0400

    React 2 Studio commit 1

--- part2/studio/src/components/AuthorInfo.jsx
+++ part2/studio/src/components/AuthorInfo.jsx
@@ -1,11 +1,32 @@
+import recipedata from "./recipe.json";
+import "./styling.css";
+
 function AuthorInfo() {
-   return;
- }
- 
- export default AuthorInfo;
- 
- //import styles.css
- //import json file for author info
- //image, name, website
- //css for styling image
- 
+  const recipeAuthorName = <div> {recipedata[0].author} </div>;
+  const recipeAuthorImage = (
+    <img
+      className="authorImage"
+      src={recipedata[0].authorImage}
+      alt={recipedata[0].recipeAuthorName}
+    />
+  );
+
+  let url = recipedata[0].website;
+  const recipeWebsite = (
+    <a href={url} target="_blank">
+      {" "}
+      {url}{" "}
+    </a>
+  );
+  // target="_blank" opens in new browser tab
+
+  return (
+    <div>
+      {recipeAuthorImage}
+      {recipeAuthorName}
+      {recipeWebsite}
+    </div>
+  );
+}
+
+export default AuthorInfo;
