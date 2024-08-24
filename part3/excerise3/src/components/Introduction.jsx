accd940ebe3d57b1074572df653ad1d1eba02376
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-24 13:27:03 -0400

    React3 Exercise commit 1

---
+++ part3/excerise3/src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction() {
+  return (
+    <div>
+      <h2>3 resources for Japanese learning</h2>
+      <ol>
+        <li>WaniKani</li>
+        <li>Bunpro</li>
+        <li>NHK Easy</li>
+      </ol>
+    </div>
+  );
+}
